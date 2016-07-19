b44c35cfbc3ab4cd03f7fd71b7a800b6aef0c1e3
student <student>
2016-07-19 15:42:49 +0900

    db構造変更

--- questionnaire_system.sql
+++ questionnaire_system.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2016 年 5 朁E17 日 04:52
--- サーバのバージョン： 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: 2016 年 7 朁E19 日 08:42
+-- サーバのバージョン： 10.1.9-MariaDB
+-- PHP Version: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `questionnaire_system`
@@ -23,15 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- テーブルの構造 `answer`
+-- テーブルの構造 `answer_radio`
 --
 
-CREATE TABLE IF NOT EXISTS `answer` (
+CREATE TABLE `answer_radio` (
   `id` int(11) NOT NULL,
   `questionnaire_id` int(11) NOT NULL,
-  `answer` int(11) NOT NULL,
-  `respondent` int(11) NOT NULL,
+  `answer` int(1) NOT NULL,
+  `respondent_id` int(2) NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `answer_text`
+--
+
+CREATE TABLE `answer_text` (
+  `id` int(11) NOT NULL,
+  `questionnaire_id` int(11) NOT NULL,
+  `answer` text NOT NULL,
+  `respondent_id` int(2) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -40,26 +54,15 @@ CREATE TABLE IF NOT EXISTS `answer` (
 -- テーブルの構造 `evaluations`
 --
 
-CREATE TABLE IF NOT EXISTS `evaluations` (
-  `id` int(10) unsigned NOT NULL,
-  `product_id` int(10) unsigned NOT NULL,
-  `age_group` int(5) unsigned NOT NULL,
+CREATE TABLE `evaluations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` varbinary(64) NOT NULL,
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `age_group` int(2) UNSIGNED NOT NULL,
   `gender` tinyint(1) NOT NULL,
-  `value` int(5) unsigned NOT NULL,
   `impressions` text NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-
---
--- テーブルのデータのダンプ `evaluations`
---
-
-INSERT INTO `evaluations` (`id`, `product_id`, `age_group`, `gender`, `value`, `impressions`, `create_time`) VALUES
-(1, 1, 2, 1, 5, 'よかったです', '2016-05-01 21:41:20'),
-(2, 3, 1, 0, 3, 'まあまあ', '2016-05-01 23:28:12'),
-(4, 1, 4, 1, 2, 'aaa', '2016-05-02 00:43:10'),
-(5, 1, 4, 0, 3, 'wwwww', '2016-05-02 05:14:26'),
-(6, 2, 4, 1, 3, 'cccccccc', '2016-05-05 09:02:24');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -67,11 +70,11 @@ INSERT INTO `evaluations` (`id`, `product_id`, `age_group`, `gender`, `value`, `
 -- テーブルの構造 `members`
 --
 
-CREATE TABLE IF NOT EXISTS `members` (
+CREATE TABLE `members` (
   `id` int(11) NOT NULL,
-  `product_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) UNSIGNED NOT NULL,
   `name` varchar(128) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `members`
@@ -90,23 +93,24 @@ INSERT INTO `members` (`id`, `product_id`, `name`) VALUES
 -- テーブルの構造 `products`
 --
 
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(10) unsigned NOT NULL,
+CREATE TABLE `products` (
+  `id` int(10) UNSIGNED NOT NULL,
   `product_name` varchar(128) NOT NULL,
   `overview` text NOT NULL,
   `img` varchar(128) DEFAULT NULL,
+  `delegate` varchar(256) NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `products`
 --
 
-INSERT INTO `products` (`id`, `product_name`, `overview`, `img`, `create_time`) VALUES
-(1, 'イラスト', '夜景を書きました。色使いに意識して尚且つ建物もらしさを意識して描いてみました。コンセプトは「明け方」ということなので、雰囲気作りに苦労はしましたが、うまく表現出来たと思います。', 'img/480_800', '2016-04-27 06:21:29'),
-(2, 'コミック', 'ここを頑張りました', NULL, '2016-04-27 06:23:16'),
-(3, 'ゲーム', '作りました', NULL, '2016-05-01 23:24:32'),
-(4, 'ロボット', 'ロボットを作りました', NULL, '2016-05-12 06:11:19');
+INSERT INTO `products` (`id`, `product_name`, `overview`, `img`, `delegate`, `create_time`) VALUES
+(1, 'イラスト', '夜景を書きました。色使いに意識して尚且つ建物もらしさを意識して描いてみました。コンセプトは「明け方」ということなので、雰囲気作りに苦労はしましたが、うまく表現出来たと思います。', 'img/480_800', '佐藤', '2016-04-27 06:21:29'),
+(2, 'コミック', 'ここを頑張りました', NULL, '田中', '2016-04-27 06:23:16'),
+(3, 'ゲーム', '作りました', NULL, '田中', '2016-05-01 23:24:32'),
+(4, 'ロボット', 'ロボットを作りました', NULL, '田中', '2016-05-12 06:11:19');
 
 -- --------------------------------------------------------
 
@@ -114,24 +118,21 @@ INSERT INTO `products` (`id`, `product_name`, `overview`, `img`, `create_time`) 
 -- テーブルの構造 `questionnaire`
 --
 
-CREATE TABLE IF NOT EXISTS `questionnaire` (
+CREATE TABLE `questionnaire` (
   `id` int(11) NOT NULL,
   `content` text NOT NULL,
+  `year` year(4) NOT NULL,
+  `type` varchar(12) NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- テーブルの構造 `questionnaire_year`
+-- テーブルのデータのダンプ `questionnaire`
 --
 
-CREATE TABLE IF NOT EXISTS `questionnaire_year` (
-  `id` int(11) NOT NULL,
-  `questionnaire_id` int(11) NOT NULL,
-  `year` int(4) NOT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `questionnaire` (`id`, `content`, `year`, `type`, `create_time`) VALUES
+(1, '会場はどうでしたか', 2017, 'radio', '2016-07-19 06:41:11'),
+(2, '生徒の態度はどうでしたか', 2018, 'text', '2016-07-19 06:41:16');
 
 -- --------------------------------------------------------
 
@@ -139,27 +140,42 @@ CREATE TABLE IF NOT EXISTS `questionnaire_year` (
 -- テーブルの構造 `respondents`
 --
 
-CREATE TABLE IF NOT EXISTS `respondents` (
+CREATE TABLE `respondents` (
   `id` int(11) NOT NULL,
-  `respondent` int(11) NOT NULL,
+  `respondent` varchar(24) NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `respondents`
+--
+
+INSERT INTO `respondents` (`id`, `respondent`, `create_time`) VALUES
+(1, '一般', '2016-07-19 05:54:12'),
+(2, '新入生', '2016-07-19 05:54:12'),
+(3, '企業', '2016-07-19 05:54:21');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `answer`
+-- Indexes for table `answer_radio`
 --
-ALTER TABLE `answer`
+ALTER TABLE `answer_radio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `answer_text`
+--
+ALTER TABLE `answer_text`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `evaluations`
 --
 ALTER TABLE `evaluations`
-  ADD PRIMARY KEY (`id`);
+  ADD UNIQUE KEY `user_id` (`user_id`,`product_id`);
 
 --
 -- Indexes for table `members`
@@ -180,12 +196,6 @@ ALTER TABLE `questionnaire`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `questionnaire_year`
---
-ALTER TABLE `questionnaire_year`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `respondents`
 --
 ALTER TABLE `respondents`
@@ -196,40 +206,35 @@ ALTER TABLE `respondents`
 --
 
 --
--- AUTO_INCREMENT for table `answer`
+-- AUTO_INCREMENT for table `answer_radio`
 --
-ALTER TABLE `answer`
+ALTER TABLE `answer_radio`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `evaluations`
+-- AUTO_INCREMENT for table `answer_text`
 --
-ALTER TABLE `evaluations`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+ALTER TABLE `answer_text`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `questionnaire`
 --
 ALTER TABLE `questionnaire`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `questionnaire_year`
---
-ALTER TABLE `questionnaire_year`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `respondents`
 --
 ALTER TABLE `respondents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
