3741d8dc724c27d48af8c20f73fab312dea63816
たけ <takehphp525@gmail.com>
2016-11-17 17:11:02 +0900

    作品詳細をタブ

--- weare/assets/script/script.js
+++ weare/assets/script/script.js
@@ -3,3 +3,51 @@
 		$('.drawer').drawer();
 	});
 });
+
+
+$(function(){
+  var $jsTabs = $('.js-tabs');
+  var $jsTabsLi = $('.js-tabs li');
+  var tabsLiLen = $jsTabsLi.length;
+  var tabsLiWid = $jsTabsLi.eq(0).width();
+  //タブエリアの横幅指定
+  $jsTabs.css('width',tabsLiWid * tabsLiLen);
+});
+
+$(function(){
+  var ACTIVE_SELECTOR = '.nav-tabs > .active';
+
+  var $jsTabs = $('.js-tabs');
+  var $jsTabsLi = $('.js-tabs li');
+
+  var $jsSwipe = $('.js-swipe');
+  var $scrollContainer = $('.nav-tabs-outer');
+
+  var tabsLiLen = $jsTabsLi.length;
+  var tabsLiWid = $jsTabsLi.eq(0).width();
+
+//   //タブエリアの横幅指定
+//   $jsTabs.css('width',tabsLiWid * tabsLiLen);
+//
+//   //スワイプイベント登録
+//   $jsSwipe.hammer().on('swipeleft',next);  //--------C
+//   $jsSwipe.hammer().on('swiperight',prev);
+//
+//   function next() {
+//     tabManager($(ACTIVE_SELECTOR).next('li'));
+//   }
+//   function prev() {
+//     tabManager($(ACTIVE_SELECTOR).prev('li'));
+//   }                                        //--------C
+//
+//   // 指定されたタブをカレントし要素にスクロールする
+//   function tabManager($nextTarget){
+//     $nextTarget.find('a').trigger('click');  //--------D
+//
+//     if($nextTarget.index() != -1){
+//       $scrollContainer.scrollLeft($nextTarget.index() * tabsLiWid);  //--------E
+//
+//     }
+//   }
+//
+// });
